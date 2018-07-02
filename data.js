c99b89c753a845c7b728301b0932be3df9c2ee44
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2018-07-02 15:31:18 +0200

    uploaded base of site

---
+++ data.js
@@ -0,0 +1,68 @@
+var intro = {
+  img: "./images/profile_picture.jpeg",
+  title: "Hei!",
+  nl: {
+    text: `Ik ben Suzanna Wentzel. Voor mijn minor ben ik vijf maanden<br>
+      in het buitenland, op exchange in University of Agder (Noorwegen).<br>
+      Hier ga ik de vakken "Scripting and Hacking" en "Application Development" <br>
+      volgen. Daarnaast ben ik van plan om de hele omgeving te ontdekken. <br>
+      Hier zal ik de filmpjes, foto's en verhalen van mijn avontuur plaatsen.<br>
+      Veel plezier met het bekijken ervan!`
+  },
+  en: {
+    text: `My name is Suzanna Wentzel. I am on exchange in Norway at UiA for <br>
+      my minor for five months, where I will follow the courses "Scripting <br>
+      and Hacking" and "Application Development". In addition to that, I will<br>
+      explore the surroundings and hopefully find awesome stories to share.<br>
+      On this webpage I will share my videos, photos and stories about this<br>
+      adventure. Have fun reading it!`
+  }
+}
+
+var blogposts = [
+  {
+    date: "00/00/0000",
+    nl: {
+      title: "hallo",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br>
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br>
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut <br>
+        aliquip ex ea commodo consequat. Duis aute irure dolor in <br>
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat <br>
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt<br>
+        in culpa qui officia deserunt mollit anim id est laborum.`
+    },
+    en: {
+      title: "hello",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br>
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br>
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut <br>
+        aliquip ex ea commodo consequat. Duis aute irure dolor in <br>
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat <br>
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt<br>
+        in culpa qui officia deserunt mollit anim id est laborum.`
+    }
+  },{
+    date: "00/00/0000",
+    nl: {
+      title: "hallo",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br>
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br>
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut <br>
+        aliquip ex ea commodo consequat. Duis aute irure dolor in <br>
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat <br>
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt<br>
+        in culpa qui officia deserunt mollit anim id est laborum.`
+    },
+    en: {
+      title: "hello",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br>
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br>
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut <br>
+        aliquip ex ea commodo consequat. Duis aute irure dolor in <br>
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat <br>
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt<br>
+        in culpa qui officia deserunt mollit anim id est laborum.`
+    }
+  }
+]
