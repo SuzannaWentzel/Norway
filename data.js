946075d779579738c01973f445e4f72672c0948d
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2018-08-14 18:00:09 +0200

    added the post Arrival, minor changes in Luggage

--- data.js
+++ data.js
@@ -28,18 +28,87 @@ var blogposts = [
       title: "Bagage",
       text: `Vandaag ben ik hard bezig geweest met de laatste voorbereidingen <br>
         voor mijn reis. De koffer moest dicht, wat super moeilijk was aangezien <br>
-        ik voor vijf maanden moest pakken (zie de foto hiernaast). Het is <br>
-        uiteindelijk gelukt en hij puilt niet eens zo heel erg uit. Morgen begint <br>
-        de reis, ik ben heel benieuwd naar de mensen, het landschap en de uni, <br>
-        ik ben er klaar voor!`
+        ik voor vijf maanden moest pakken (zie de foto). Het is <br>
+        gelukt uiteindelijk en hij puilt niet eens zo heel erg uit. Morgen begint <br>
+        de reis, ik ben heel benieuwd naar de mensen, het landschap en de uni. <br>
+        Ik ben er klaar voor!`
     },
     en: {
       title: "Luggage",
-      text: `Today I did the last preparations for my journey. My suitcase should <br>
-        be able to close, which was super hard since I had to pack for five <br>
-        months (see the picture on the right). Eventually it closed and it&#39s not <br>
-        even that full. Tomorrow the journey actually begins, I&#39m very curious <br>
-        for the people, the landscapes and the university, I&#39m ready to go!`
+      text: `Today I worked hard on the last preparations for my journey. My <br>
+        suitcase should be able to close, which was super hard since I had to <br>
+        pack for five months (see the picture). Eventually I closed it and it&#39s <br>
+        not even that full. Tomorrow the journey actually begins. I am a little <br>
+        bit scared, but I&#39m also very curious for the people, the landscapes <br>
+        and the university. I&#39m ready to go!`
+    }
+  }, {
+    date: "09/08/2018",
+    img: "./images/arrival.jpeg",
+    vid: "",
+    nl: {
+      title: "Aankomst",
+      text: `Vanochtend zou ik nog alle tijd hebben om mijn handbagage in te <br>
+        pakken, en nog gezellig met mijn ouders wat te drinken of een <br>
+        spelletje te spelen. Dat ging niet helemaal als gepland, dus toen was <br>
+        ik uiteindelijk net op tijd klaar met alles. Toen gingen we naar het <br>
+        vliegveld en mijn buik zat vol met kriebels. Daar aangekomen waren er <br>
+        wat vriendinnetjes om me uit te zwaaien wat super lief was, en <br>
+        natuurlijk Rik (en zijn uitzwaai-mensen) waarmee ik naar Noorwegen <br>
+        ben gegaan.<br><br>
+        De vlucht was van Amsterdam naar Kristiansand en duurde maar een uur<br>
+        en twintig minuten, maar helaas was die vlucht een half uur vertraagd..<br>
+        We hebben wel al een hoop mensen op het vliegveld ontmoet die ook naar <br>
+        UiA (onze universiteit) gingen. Helaas gingen zij allemaal naar de <br>
+        andere campus, degene in Kristiansand. Om half zeven zijn we eindelijk <br>
+        in Kristiansand aan gekomen en toen zijn we naar Grimstad gereisd, waar <br>
+        onze campus is. Het landschap is daar zo enorm anders en zo mooi!<br>
+        Overal heuvels en rotsen, een hele hoop naaldbomen en tunnels door de <br>
+        heuvels, ik keek mijn ogen uit.<br><br>
+        Eenmaal daar aangekomen zijn we opgevangen door de Buddies, de <br>
+        geweldige mensen die ons helpen settelen en die de hele groep van <br>
+        uitwisselingsstudenten begeleiden. Zij brachten ons naar onze kamers, <br>
+        die op vijf minuten lopen zijn van het hoofdgebouw. Ik zit in een <br>
+        appartement met een ander meisje, we delen een keuken en een badkamer <br>
+        en hebben allebei een eigen kamer. <br><br>
+        Nadat we onze koffers en tassen naar de kamer gesleept hadden, zijn we <br>
+        met een groep gaan zwemmen in de zee. Die is op een kwartiertje lopen <br>
+        van de campus, dus dat is super fijn. Het water was misschien wel een <br>
+        beetje koud, maar dat was heerlijk verfrissend. Het was ook zo helder, <br>
+        je kon echt ver onder je kijken. <br><br>
+        Na het zwemmen was het ondertussen alweer half tien en we hadden nog <br>
+        steeds niks gegeten, dus toen zijn we maar snel boodschappen gaan doen <br>
+        en wat gaan eten.`
+    },
+    en: {
+      title: "Arrival",
+      text: `This morning I should have had all the time to pack my hand luggage <br>
+        and have a quick chat and some tea with my parents or play a card game. <br>
+        Unfortunately it didn&#39t really go according to plan, so eventually I was <br>
+        done just in time. Then we went to the airport and there where tingles <br>
+        in my stomach. When we got there, my friends where there to wave <br>
+        goodbye, which was super sweet. Of course Rik (and his family) was <br>
+        there, since we went to Norway together. <br><br>
+        The flight was from Amsterdam to Kristiansand and took only one hour <br>
+        and twenty minutes, but unfortunately, it was delayed for half an hour.. <br>
+        We did meet a lot of people who are also going to UiA (our university) <br>
+        on the airport, but they were all going to the other campus in <br>
+        Kristiansand. At half past six we finally arrived in Kristiansand and <br>
+        took the bus to Grimstad, where our campus is located. The landscape <br>
+        here is so different from home, it&#39s so beautiful! Everywhere you look <br>
+        you can see rocks and trees (so many trees) and tunnels through the <br>
+        hills, amazing.<br><br>
+        When we got there, we were received by the Buddies, the amazing people <br>
+        that will help us settle and guide the whole group of exchange <br>
+        students. They brought us to our rooms, five minutes walking from the <br>
+        main building of the university. I am sharing an apartment with another <br>
+        girl, we share a kitchen and a bathroom and both have our own bedroom.<br><br>
+        After we dragged our suitcases and backpacks to our rooms, we went <br>
+        swimming in the sea with some other people. The sea is fifteen minutes <br>
+        walking from campus, which is really nice. It was a little bit cold maybe, <br>
+        but it was so refreshing and clear, you could see very deep beneath you.<br><br>
+        After the swim it was already half past nine, and we still had not eaten <br>
+        anything, so we went getting groceries and had some food.`
     }
   }
 ]
